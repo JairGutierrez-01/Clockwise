c8e209d8fe2e6de4a96d47b51fd70093730cc3d0
y0114410 <c.ebben@tu-braunschweig.de>
2025-05-12 23:27:41 +0200

    theme.js: JSDocs style

--- frontend/static/js/theme.js
+++ frontend/static/js/theme.js
@@ -1,5 +1,9 @@
-// static/theme.js
+// static/js/theme.js
 
+/**
+ * Toggles between light and dark mode by modifying body classes.
+ * Switches the visibility of the sun and moon icon accordingly.
+ */
 function toggleTheme() {
   const body = document.body;
   const sunIcon = document.getElementById("sun-icon");
@@ -15,7 +19,10 @@ function toggleTheme() {
   }
 }
 
-// Direkt beim Laden den korrekten Icon anzeigen
+/**
+ * Sets the initial state of the sun/moon icon when the page loads,
+ * based on whether dark mode is activated.
+ */
 document.addEventListener("DOMContentLoaded", () => {
   const sunIcon = document.getElementById("sun-icon");
   const moonIcon = document.getElementById("moon-icon");
